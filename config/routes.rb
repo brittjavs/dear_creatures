a987e4e3ee747e464875b6c1f411aa492d71f3db
Brittany Javalera <brittjavalera@gmail.com>
2019-12-05 14:55:31 -0800

    created signin and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/signin' to: "session#new"
+  post '/signin' to: "sessions#create"
+  get '/logout', to: 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
