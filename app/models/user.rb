e3c9e3d448541c3a76f462a223f8c2d71934db96
Brittany Javalera <brittjavalera@gmail.com>
2019-12-05 15:56:56 -0800

    added email to user signup form and added validations for email and username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :donations
     has_many :shelters, through: :donations
     has_secure_password
+    validates :email, :username, presence: true
+    validates :email, :username, uniqueness: true
+
 end
