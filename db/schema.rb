50b87159e4d267b7950ae2e1ebb5a76aed87d6c1
Brittany Javalera <brittjavalera@gmail.com>
2019-12-07 15:38:40 -0800

    added a description column to shelters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_061641) do
+ActiveRecord::Schema.define(version: 2019_12_07_233631) do
 
   create_table "donations", force: :cascade do |t|
     t.integer "shelter_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_12_07_061641) do
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
