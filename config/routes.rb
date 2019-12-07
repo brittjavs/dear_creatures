eb547489f3dcb4ec4f413d5dba38501addec4eb7
Brittany Javalera <brittjavalera@gmail.com>
2019-12-07 14:41:16 -0800

    Added links to shelter show page as well as new donation page on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :donations
-  resources :shelters
   root "static#home"
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/logout', to: 'sessions#destroy'
   get '/signup', to: "users#new"
   resources :users
+  resources :donations
+  resources :shelters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
