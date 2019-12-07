6bf07a76f601357be09b13f2edf5e24f48ab6985
Brittany Javalera <brittjavalera@gmail.com>
2019-12-07 15:36:03 -0800

    created donations new view and shelter index and show views

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,5 +2,5 @@ class Shelter < ApplicationRecord
     has_many :donations
     has_many :needed_items
     has_many :users, through: :donations
-    has_many :items, thourgh: :needed_items
+    has_many :items, through: :needed_items
 end
