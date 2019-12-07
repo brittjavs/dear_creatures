2c0f180b3de8cf3633af6f5a4caf530455686df3
Brittany Javalera <brittjavalera@gmail.com>
2019-12-06 18:08:05 -0800

    updated @user value in sessions#create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create
+        # binding.pry
      @user = User.new(user_params)
         if @user.save
             session[:user_id]=@user.id
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find_by(params[:id])
+        @user = User.find(params[:id])
     end
 
 
