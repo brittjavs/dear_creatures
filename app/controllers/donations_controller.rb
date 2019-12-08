3a1e6a56d2b8881f922e5b865fde9392385d5f00
Brittany Javalera <brittjavalera@gmail.com>
2019-12-07 23:03:46 -0800

    created donation params and added shelter drop down in the donation form

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,15 @@
 class DonationsController < ApplicationController
+    def new
+        @donation = Donation.new
+    end
+
+    def create
+
+            
+    end
+
+    private
+    def donation_params
+        params.require(:donation).permit(:shelter_id, :user_id, :item_id, :quantity)
+    end
 end
