3c80ecc5d5b14701b860fb607e79eff517b158a6
Brittany Javalera <brittjavalera@gmail.com>
2019-12-08 12:58:52 -0800

    added a nested resource so viewers will be able to see all their past donations

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   resources :users
   resources :donations
   resources :shelters
+  resources :users, only: [:show] do
+    resources :donations, only: [:index]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
