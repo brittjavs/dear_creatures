f1cfcaa29b07c1113424b594f309013d4545288f
Brittany Javalera <brittjavalera@gmail.com>
2019-12-11 22:17:18 -0800

    added omniauth gems for third party login through google. To create connection to google I added .env file, omniauth initializer, User.from_omniauth method in user model, omniauth method in sessions controller, added link in home page to log in with google, and created route to sessions#omniauth using callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,33 @@ class SessionsController < ApplicationController
     def new
     end
 
+    def omniauth
+        @user = User.from_omniauth(auth)
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
+
     def create
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
+            flash[:error] = "Sorry, login info was incorrect. Please try again."
             render :new
         end
     end
+
 
     def destroy
         session.clear
         redirect_to '/'
     end
+
+    private
+     def auth
+        request.env['omniauth.auth']
+     end
+
 end
