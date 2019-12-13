4f0bcb5ca354070ba8fe4bb126c296349a4abc70
Brittany Javalera <brittjavalera@gmail.com>
2019-12-12 21:28:27 -0800

    added error message to signup form. Added links for home and logout in application view that will display if user is current_user

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -4,4 +4,11 @@ class Shelter < ApplicationRecord
     has_many :users, through: :donations
     has_many :items, through: :needed_items
 
+    # def update_item_qty
+        # def update_quantity(shelter_id, item_id, quantity)
+            #     item = NeededItem.where(:shelter_id => shelter_id, :item_id => item_id)
+            #     updated_count = item.quantity -= quantity
+            #     item.update(:qty => updated_count)
+            # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_many :donations
     has_many :shelters, through: :donations
-    has_secure_password
     validates :email, :username, presence: true
     validates :email, :username, uniqueness: true
-
+    has_secure_password
+    
     def self.from_omniauth(auth)
         where(email: auth.info.email).first_or_initialize do |user|
           user.username = auth.info.name
