0aea982bbb3cf729bf5281fad13a9efce6be40d2
Brittany Javalera <brittjavalera@gmail.com>
2019-12-18 09:16:26 -0800

    added flash error if user submits quantity and dollar amount

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -12,15 +12,17 @@ class DonationsController < ApplicationController
             if params[:donation][:quantity].empty? && params[:donation][:dollar_amount].empty?
                 flash[:error] = "Please fill out quantity if donating an item or dollar amount if donating funds"
                 render :new
+            elsif !params[:donation][:quantity].empty? && !params[:donation][:dollar_amount].empty?
+                flash[:warning] = "Please fill out only one - quantity if donating an item or dollar amount if donating funds"
+                render :new
             else 
                 if params[:donation][:quantity].empty?
                     NeededItem.update_funds(params[:donation][:shelter_id], params[:donation][:item_id], params[:donation][:dollar_amount])
                 else
                     NeededItem.update_quantity(params[:donation][:shelter_id], params[:donation][:item_id], params[:donation][:quantity])
                 end
+            redirect_to shelter_path(@donation.shelter)
             end
-         redirect_to shelter_path(@donation.shelter)
-
         end
     end
 
